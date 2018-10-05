72b797d44908f4b4627432b0968ac1f2100e6d6a
D4vidz <zengzhiyuan1@qq.com>
2018-10-05 11:57:23 +0800

    栈

---
+++ python_data_structure/stack.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Stack(object):
+
+    def __init__(self, *args):
+        self.__list = list(args)
+
+    def __str__(self):
+        return str(self.__list)
+
+    def push(self, item):
+        self.__list.append(item)
+
+    def pop(self):
+        self.__list.pop(-1)
+
+    def get_count(self):
+        return self.__list.__len__()
+
+    def is_empty(self):
+        if self.__list:
+            return False
+        return True
+
+
+if __name__ == '__main__':
+    demo = Stack(1, 2, 3, 4)
+    print(demo)
+    demo.push(5)
+    print(demo)
+    demo.pop()
+    print(demo)
+    print(demo.get_count())
+    print(demo.is_empty())
