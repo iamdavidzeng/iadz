01ab2f7ef22d51b3b900da1b6c7353923347a2b7
zzy <15779712170@163.com>
2019-01-20 12:11:55 +0800

    user __import__ get func from another file.

---
+++ python_os_sys/handles.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def first():
+    return 'first'
+
+
+def second():
+    return 'second'
+
+
+def _third():
+    return 'third'
---
+++ python_os_sys/import_demo.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+def import_func(module_name):
+    num = module_name.rfind('.')
+    if num == -1:
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[num+1:]
+        mod = __import__(module_name[:num], globals(), locals())
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            print("it's not what i want: %s" % attr)
+            continue
+        func = getattr(mod, attr)
+        if callable(func):
+            print("this's what i want: %s" % func.__name__)
+
+
+if __name__ == '__main__':
+    import_func('handles.py')
