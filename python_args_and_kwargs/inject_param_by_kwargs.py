53e2f34473f4f1ac8d2364eaf9236268dae9b3d9
David Zeng <david.zeng@student.com>
2019-01-28 14:24:59 +0800

    use kwargs inject params.

---
+++ python_args_and_kwargs/inject_param_by_kwargs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def func_for_test(enquiry):
+    print(enquiry)
+
+
+if __name__ == '__main__':
+    data = {'first': 'January'}
+
+    func_for_test(enquiry=data)
