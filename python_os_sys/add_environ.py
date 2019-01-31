768552fb875b547d4f4d18dcc5fcb181cda87892
David Zeng <david.zeng@student.com>
2019-01-31 16:00:52 +0800

    try to use os change environ.

---
+++ python_os_sys/add_environ.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+# 添加上级目录到当前文件的环境变量当中
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+if __name__ == '__main__':
+    environ_lst = (
+        os.path.dirname(__file__),
+        os.path.join(os.path.dirname(__file__), '..'),
+        os.path.abspath(os.path.join(os.path.dirname(__file__), '..')),
+    )
+    for environ in environ_lst:
+        print(environ)
