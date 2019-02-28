806871eccfe9362db5edc0f2b3ca831969527de8
zzy <15779712170@163.com>
2019-02-28 22:36:42 +0800

    -_-

---
+++ _marshmallow/_schema/user.py
@@ -0,0 +1,58 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from functools import wraps
+
+from marshmallow import (
+    Schema, fields, post_dump, post_load
+)
+
+
+class User(Schema):
+
+    name = fields.Str(required=False, allow_none=True)
+    address = fields.Str(required=False, allow_none=True)
+    password = fields.Str(required=False, allow_none=True)
+    # read-only
+    first_name = fields.Str(dump_only=True)
+    # write-only
+    last_name = fields.Str(load_only=True)
+
+    # @post_load()
+    # def add_gender_to_data(self, data):
+    #     data['gender'] = data.get('gender') or None
+
+
+def validate_input(schema):
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print('data: %s' % args)
+            args_list = list(args)
+            args_list[0] = schema(strict=True).load(args[0]).data
+            return func(*args_list, **kwargs)
+        return wrapper
+    return decorator
+
+
+@validate_input(User)
+def validate_user(data):
+    print('data_after_load: %s' % data)
+    return data
+
+
+if __name__ == '__main__':
+    mock_student = {
+        'name': 'mock_name',
+        'address': 'mock_address',
+        'password': 'mock_password',
+        'first_name': 'David',
+        'last_name': 'Zeng',
+    }
+    data_after_load = validate_user(mock_student)
+
+    data_after_dump = User(strict=True).dump(mock_student).data
+
+    print('data_after_dump: %s' % data_after_dump)
---
+++ python_nameko/test/test_user_integration_test.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+import pytest
+
+from nameko.rpc import rpc, RpcProxy
+from nameko.testing.utils import get_container
+from nameko.testing.services import entrypoint_hook
+
+
+class ServiceX:
+
+    name = 'service_x'
+
+    y = RpcProxy('service_y')
+
+    @rpc
+    def remote_method(self, value):
+        res = '{}-x'.format(value)
+        return self.y.append_identifier(res)
+
+
+class ServiceY:
+
+    name = 'service_y'
+
+    @rpc
+    def append_identifier(self, value):
+        return '{}-y'.format(value)
+
+
+def test_service_x_y_integration(runner_factory, rabbit_config):
+
+    runner = runner_factory(rabbit_config, ServiceX, ServiceY)
+    runner.start()
+
+    container = get_container(runner, ServiceX)
+    with entrypoint_hook(container, 'remote_method') as entrypoint:
+        assert entrypoint('value') == 'value-x-y'
