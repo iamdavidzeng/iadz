d39fe8065635431d1a810dc53c0ff9a809dd1ca4
zzy <15779712170@163.com>
2019-03-14 23:46:33 +0800

    use SQLAlchemy and alembic initial table

---
+++ _sqlalchemy/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Person(Base):
+
+    __tablename__ = 'person'
+
+    id = Column(Integer, primary_key=True)
+
+    nickname = Column(String(32), nullable=True)
+    first_name = Column(String(32), nullable=True)
+    last_name = Column(String(32), nullable=True)
