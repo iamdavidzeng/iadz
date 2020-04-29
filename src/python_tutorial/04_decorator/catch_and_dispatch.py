3df01c4fbfe8fd806e4e5bf9c1fade7c32107424
iamdavidzeng <15779712170@163.com>
2020-04-29 12:10:30 +0800

    decorator demo

---
+++ src/python_tutorial/04_decorator/catch_and_dispatch.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+from functools import wraps
+
+
+class CustomizeException(Exception):
+
+    pass
+
+
+def catch_and_dispatch(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:
+            print("i am here...")
+            raise exc
+
+    return wrapper
+
+
+@catch_and_dispatch
+def raise_an_error():
+    raise CustomizeException()
+
+
+if __name__ == "__main__":
+    raise_an_error()
