d2021e73b29fa6ac226088d4641adde64d89b1ea
iamdavidzeng <15779712170@163.com>
2020-05-06 18:10:34 +0800

    inspect param default value

---
+++ src/python_tutorial/07_inspect/inspect_params.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import inspect
+
+def func1(name, gender="male", *args, **kwargs):
+    pass
+
+
+if __name__ == "__main__":
+    signature = inspect.signature(func1)
+    
+    for k, v in signature.parameters.items():
+        print(f"param_name: {k}, deafault_value: {v.default}")
