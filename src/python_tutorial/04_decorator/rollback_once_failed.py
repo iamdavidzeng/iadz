6aee00a5b0d15f09b276487e8b87444dbdf5079a
iamdavidzeng <15779712170@163.com>
2020-05-06 17:52:46 +0800

    decorator get func deafult params

--- src/python_tutorial/04_decorator/rollback_once_failed.py
+++ src/python_tutorial/04_decorator/rollback_once_failed.py
@@ -4,16 +4,29 @@
 在涉及到session的部分，
 为了保证数据的一致性，在raise的时候进行session.rollback()
 """
-
+import inspect
 from functools import wraps
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+def get_default_args(func):
+    signature = inspect.signature(func)
+    return {
+        k: v.default
+        for k, v in signature.parameters.items()
+        if v.default is not inspect.Parameter.empty
+    }
+
+
 def rollback_once_failed(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
+        default_kws = get_default_args(func)
+        kwargs.update(default_kws)
+
+        commit = kwargs.get("commit")
         try:
             func(self, *args, **kwargs)
         except Exception as exc:
@@ -21,8 +34,10 @@ def rollback_once_failed(func):
             print("commit failed, rollback")
             raise exc
         else:
-            self.session.commit()
-            print("commit successfully")
+            if commit:
+                self.session.commit()
+                print("commit successfully")
+            print("func can't commit when commit is False")
     return wrapper
 
 
@@ -35,12 +50,13 @@ class BillingService(object):
         self.session = session
 
     @rollback_once_failed
-    def create_item(self):
+    def create_item(self, commit=True):
         self.session.execute(
             """
-            INSERT INTO `user_role` (`user_id`, `role`)
-            VALUES
-                (1, 'partner');
+            INSERT INTO `user` (`email`, `password`, `email_verified`, `email_token`, `newsletter_accepted`, `forgot_password_token`, `forgot_password_token_issued_at`, `password_set_by_user`, `create_password_token`, `osl_password_key_pair`, `uuid`, `created_at`, `updated_at`, `facebook_id`, `wechat_id`, `phone`, `enabled`, `role_data`)
+VALUES
+	('hui.zhao2@student.com', NULL, 0, NULL, 0, NULL, NULL, 0, NULL, NULL, 'd5d93672-2947-9207-cd44-c7e7e8ba4f72', '2018-02-06 10:00:00', '2018-02-06 10:00:00', NULL, NULL, NULL, 1, NULL);
+
             """
         )
         raise InsertError("Whoops, Error!")
@@ -53,4 +69,4 @@ if __name__ == "__main__":
     session = Session()
 
     billing = BillingService(session=session)
-    billing.create_item()
+    billing.create_item(commit=False)
