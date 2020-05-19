5a474aca946f17e83edeb921e473ccf1b15a52c1
iamdavidzeng <15779712170@163.com>
2020-05-19 23:05:28 +0800

    marshmallow datetime load and dump

--- src/tutorial/09_marshmallow/validate_datetime.py
+++ src/tutorial/09_marshmallow/validate_datetime.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from marshmallow import Schema, fields
 
 
@@ -11,9 +13,12 @@ if __name__ == "__main__":
 
     specific_moment = {
         "invoce_datetime": "2020-5-12 10:00:00",
-        "due_datetime": "2020-5-12 10:00:00",
+        "due_datetime": datetime.utcnow().isoformat(),
     }
 
     load_sm = TimeLine(strict=True).load(specific_moment).data
+    print(f"load_sm: {load_sm}")
 
-    print(load_sm)
+    dump_sm = TimeLine(strict=True).dump(load_sm).data
+    print(f"dump_sm: {dump_sm}")
+
