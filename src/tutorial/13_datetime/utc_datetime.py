25342a6e2bbda2cf2061d29da28d50557b154917
iamdavidzeng <15779712170@163.com>
2020-07-01 19:16:43 +0800

    multiple timezone

--- src/tutorial/13_datetime/utc_datetime.py
+++ src/tutorial/13_datetime/utc_datetime.py
@@ -2,10 +2,13 @@
 
 import time
 
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 
 if __name__ == "__main__":
 
     utcnow = datetime.utcnow()
-    print(f"second utcnow: {utcnow.isoformat()}")
+    print(f"UTC+00:00 now: {utcnow.isoformat()}")
+
+    utc6_now = datetime.now(tz=timezone(offset=timedelta(hours=6)))
+    print(f"UTC+06:00 now: {utc6_now.isoformat()}")
