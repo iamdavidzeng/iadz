e26b154ab8b527aff75d64ef46cb2f11709309d6
iamdavidzeng <15779712170@163.com>
2020-07-23 14:20:13 +0800

    Remove frontend project

--- src/frontend_tutorial/react_family_tutorial/babel.config.js
+++
@@ -1,6 +0,0 @@
-const babelConfig = {
-    presets: ["@babel/preset-react", "@babel/preset-env"],
-    plugins: ["@babel/plugin-syntax-dynamic-import"]
- }
- 
- module.exports = babelConfig;
--- src/frontend_tutorial/umi_dva_tutorial/.umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'umi';
-
-export default defineConfig({
-  locale: { antd: true },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-    { path: '/products', component: '@/pages/products' },
-  ],
-});
