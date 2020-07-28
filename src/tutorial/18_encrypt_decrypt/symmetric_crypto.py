65de2388e6ca231e8512e66c80c6eb44a3a5f6d7
iamdavidzeng <15779712170@163.com>
2020-07-28 18:29:03 +0800

    Restructure crypto

--- src/tutorial/18_encrypt_decrypt/symmetric_crypto.py
+++ src/tutorial/18_encrypt_decrypt/symmetric_crypto.py
@@ -2,70 +2,53 @@
 import random, string, base64
 
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 
 def generate_private_key():
     return "".join(random.sample(string.ascii_letters + string.digits, 32))
 
 
-class CryptoManagement:
-    def __init__(self, private_key):
-        self.private_key = private_key.encode("utf-8")
+def encrypt(secret_key, data):
+    secret_key = secret_key.encode("utf-8")
+    encryptor = AES.new(key=secret_key, mode=AES.MODE_CBC)
+    cipher_text = encryptor.encrypt(pad(data.encode("utf-8"), AES.block_size))
+    return base64.b64encode(cipher_text).decode("utf-8"), base64.b64encode(encryptor.iv).decode("utf-8")
 
-    def setup(self):
-        """"
-        A cipher object is stateful: once you have decrypted a message
-        you cannot decrypt (or encrypt) another message with the same
-        object.
-        """
-        self.encryptor = AES.new(
-            key=self.private_key, mode=AES.MODE_CBC, iv=self.private_key[:16]
-        )
-        self.decryptor = AES.new(
-            key=self.private_key, mode=AES.MODE_CBC, iv=self.private_key[:16]
-        )
 
-    def encrypt(self, data):
-        cipher_text = self.encryptor.encrypt(self._padding(data).encode("utf-8"))
-        return base64.b64encode(cipher_text).decode("utf-8")
+def decrypt(secret_key, data, iv):
+    secret_key = secret_key.encode("utf-8")
+    iv = base64.b64decode(iv)
+    decryptor = AES.new(key=secret_key, mode=AES.MODE_CBC, iv=iv)
 
-    def decrypt(self, data):
-        data = base64.b64decode(data)
+    data = base64.b64decode(data)
 
-        decrypted = self.decryptor.decrypt(data).decode("utf-8")
+    decrypted = decryptor.decrypt(data)
 
-        return self._unpadding(decrypted)
-
-    @staticmethod
-    def _padding(data):
-        padding_len = 16 - (len(data.encode("utf-8")) % 16)
-        data += chr(padding_len) * padding_len
-        return data
-
-    @staticmethod
-    def _unpadding(data):
-        paidding_len = len(data)
-        if paidding_len == 0:
-            return data
-        asc = ord(data[-1])
-        print(asc)
-        if asc >= 16:
-            return data
-        return data[:-asc]
+    return unpad(decrypted, AES.block_size).decode("utf-8")
 
 
 if __name__ == "__main__":
 
     text = "gibbs@student.c3"
 
-    private_key = generate_private_key()
-    print(f"private_key: {private_key}")
+    secret_key = "Cs0n1jWi6PGVXqO4dElegAt8bBzx3yaU"
+    print(f"private_key: {secret_key}")
 
-    crypto = CryptoManagement(private_key=private_key)
-    crypto.setup()
+    encrypted1, iv1 = encrypt(secret_key, "gibbs1@student.com")
+    print(f"encrypted1: {encrypted1}, iv1: {iv1}")
+    encrypted2, iv2 = encrypt(secret_key, "gibbs2@student.com")
+    print(f"encrypted2: {encrypted2}, iv2: {iv2}")
 
-    encrypted = crypto.encrypt(text)
-    print(f"encrypted: {encrypted}")
-
-    decrypted = crypto.decrypt(encrypted)
-    print(f"decrypted: {decrypted}")
+    decrypted1 = decrypt(
+        secret_key,
+        encrypted1,
+        iv1,
+    )
+    print(f"decrypted1: {decrypted1}")
+    decrypted2 = decrypt(
+        secret_key,
+        encrypted2,
+        iv2
+    )
+    print(f"decrypted2: {decrypted2}")
