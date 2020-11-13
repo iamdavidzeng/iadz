a9419b0c67da42b7bff0b3e6aa6003214f86a7a4
iamdavidzeng <15779712170@163.com>
2020-11-13 10:46:01 +0800

    Use map and lambda join list

---
+++ src/tutorial/06_dict/dict_as_argument.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from stripe.stripe_object import StripeObject
+from stripe.util import convert_to_stripe_object
+
+
+
+metadata = convert_to_stripe_object(
+    {"user_id": 1}
+)
+
+print(metadata)
+
+def update_dict(name, gender, metadata):
+
+    metadata.update(user_id=1)
+
+    print(metadata)
+
+
+if __name__ == "__main__":
+
+    metadata = {"nick": "iamdavidzeng"}
+
+    update_dict(1, 2, metadata=metadata)
+
+    print(metadata)
--- src/tutorial/08_list/join_list.py
+++ src/tutorial/08_list/join_list.py
@@ -6,8 +6,14 @@ lst_1 = [(4455.0, 'NULL'), (4455.0, 'NULL')]
 lst_2 = [(4455.0, 'NULL', 'NULL'), (4455.0, 'NULL', 'NULL')]
 
 
+lst_3 = ["david.zeng1", "david.zeng2"]
+
+
 values_1 = ",".join((str(i) for i in lst_1))
 print(f"values_1: {values_1}")
 
 values_2 = ",".join((str(i) for i in lst_2))
 print(f"values_2: {values_2}")
+
+values_3 = "".join(map(lambda x: f"<@{x}>", lst_3))
+print(f"values_3: {values_3}")
