91c6939e3aa5407bbe55544b5bea22769c5cf84d
iamdavidzeng <15779712170@163.com>
2021-03-15 14:15:51 +0800

    Use enumrate and zip

---
+++ src/tutorial/06_dict/zip_create_dict.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+subjects = ["math", "chemistry", "biology", "pyhsics"]
+grades = [100, 83, 90, 92]
+
+
+grades_dict = dict(zip(subjects, grades))
+print(grades_dict)
---
+++ src/tutorial/08_list/enumerate_list.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+words = ["Hannibal", "Hanny", "Steeve"]
+
+for index, word in enumerate(words):
+    print(index, word)
