164341041ca5b350e31e6d178262557b059bc5c2
iamdavidzeng <15779712170@163.com>
2021-06-23 17:53:24 +0800

    zip mapping dict

--- python/src/iadz/built_in/dict/zip_create_dict.py
+++ python/src/iadz/built_in/dict/zip_create_dict.py
@@ -6,3 +6,10 @@ grades = [100, 83, 90, 92]
 
 grades_dict = dict(zip(subjects, grades))
 print(grades_dict)
+
+
+
+columns = ["first_name", "last_name"]
+resource = ["David", "Zeng"]
+users = dict(zip(columns, resource))
+print(users)
