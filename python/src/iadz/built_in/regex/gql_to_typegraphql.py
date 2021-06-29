955e3ffcc93628fc9e26e1ed4fae3f8996f1f957
iamdavidzeng <15779712170@163.com>
2021-06-29 15:15:29 +0800

    Improve gql to typegraphql

--- python/src/iadz/built_in/regex/gql_to_typegraphql.py
+++ python/src/iadz/built_in/regex/gql_to_typegraphql.py
@@ -2,26 +2,28 @@
 
 
 import re
-from sre_constants import GROUPREF
 
 stream = """
 type MsgCenterLandlordContact implements Node {
-        id: ID!
-        firstName: String!
-        lastName: String!
-        userUuid: NonEmptyString!
-        liveChatEnabled: Boolean!
-        online: Boolean
-    }
+    id: ID!
+    firstName: String!
+    lastName: String!
+    userUuid: NonEmptyString!
+    liveChatEnabled: Boolean!
+    online: Boolean
+}
 
-type MsgCenterLandlordContact implements 123 {
-        id: ID!
-        firstName: String!
-        lastName: String!
-        userUuid: NonEmptyString!
-        liveChatEnabled: Boolean!
-        online: Boolean
-    }
+input MsgCenterLandlordContact implements 123 {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    userUuid: NonEmptyString!
+    liveChatEnabled: Boolean!
+    online: Boolean
+    pageNumber: Int = 1
+    pageSize: Int = 10
+    edges: [Foo]
+}
 """
 
 
@@ -30,24 +32,40 @@ def convert(match):
         lines = group.split("\n")
         for index, line in enumerate(lines):
             prefix = ""
-            if index == 0:
+            if "type" in line:
                 prefix = "@ObjectType()\n"
                 line_list = line.split(" ")
                 line = " ".join(["export class", line_list[1], line_list[-1], "\n"])
-            
+
+            if "input" in line:
+                prefix = "@ArgsType()\n"
+                line_list = line.split(" ")
+                line = " ".join(["export class", line_list[1], line_list[-1], "\n"])
+
             if ":" in line:
                 type_ = line.strip().split(" ")[1]
-                prefix = f"@Field(() => {type_})\n"
+                prefix = f"  @Field(() => {type_})\n  "
                 if "!" in line:
                     type_ = type_.replace("!", "")
                     nullable = "{ nullable: false }"
-                    prefix = f"@Field(() => {type_}, {nullable})\n"
+                    prefix = f"  @Field(() => {type_}, {nullable})\n  "
+                
+                if "[" in prefix:
+                    prefix = prefix.replace("[", "").replace("]", "[]")
 
-            new_line = prefix + line.strip().replace("!", "").replace("ID", "number").lower() + "\n"
+            new_line = (
+                prefix
+                + line.strip()
+                .replace("!", "")
+                .replace("ID", "string")
+                .replace("Int", "number")
+                .replace("NonEmptyString", "string")
+                + "\n"
+            )
             lines[index] = new_line
     return "\n".join(lines)
 
 
-parser = re.compile(r"(\w+[\s\w]+\{[\s\w\n\:\!]+\})")
+parser = re.compile(r"(\w+[\s\w]+\{[\s\w\n\:\!\=\[\]]+\})")
 result = parser.sub(convert, stream)
 print(f"result: {result}")
