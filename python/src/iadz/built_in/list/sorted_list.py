29ec4fcceb427ae5576e287da1bbaf75c9afdf76
iamdavidzeng <15779712170@163.com>
2021-07-16 11:05:08 +0800

    Escape use string as sorted's key

--- python/src/iadz/built_in/list/sorted_list.py
+++ python/src/iadz/built_in/list/sorted_list.py
@@ -1,14 +1,15 @@
+from decimal import Decimal
 foo = [
     {
-        "minimum_price": 100,
+        "minimum_price": "1234.00"
     },
     {
-        "minimum_price": 200,
+        "minimum_price": "160.00"
     },
     {
-        "minimum_price": 50,
+        "minimum_price": "159.00"
     },
 ]
 
-bar = sorted(foo, key=lambda f: f["minimum_price"], reverse=False)
+bar = sorted(foo, key=lambda f: Decimal(f["minimum_price"]), reverse=False)
 print(f"sorted: {bar}")
