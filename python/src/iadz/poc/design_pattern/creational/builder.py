223c2f6c8d60c810381f6aa03e259157c5cd1dfe
zzy <15779712170@163.com>
2021-08-15 19:25:09 +0800

    read builder

---
+++ python/src/iadz/poc/design_pattern/creational/builder.py
@@ -0,0 +1,75 @@
+# Abstract Building
+class Building:
+    def __init__(self) -> None:
+        self.build_floor()
+        self.build_size()
+
+    def build_floor(self):
+        raise NotImplementedError
+
+    def build_size(self):
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
+        return f"Floor: {self.floor} | Size: {self.size}"
+
+
+# Concrete Buildings
+class House(Building):
+    def build_floor(self):
+        self.floor = "One"
+
+    def build_size(self):
+        self.size = "Big"
+
+
+class Flat(Building):
+    def build_floor(self):
+        self.floor = "More than One"
+
+    def build_size(self):
+        self.size = "Small"
+
+
+# external constructor function
+class ComplexBuilding:
+    def __repr__(self) -> str:
+        return f"Floor: {self.floor} | Size: {self.size}"
+
+
+class ComplexHouse(ComplexBuilding):
+    def build_floor(self):
+        self.floor = "One"
+
+    def build_size(self):
+        self.size = "Big and fancy"
+
+
+def construct_building(cls):
+    building = cls()
+    building.build_floor()
+    building.build_size()
+    return building
+
+
+def main():
+    """
+
+    >>> house = House()
+    >>> house
+    Floor: One | Size: Big
+    >>> flat = Flat()
+    >>> flat
+    Floor: More than One | Size: Small
+
+    # Using an external constructor function:
+    >>> complex_house = construct_building(ComplexHouse)
+    >>> complex_house
+    Floor: One | Size: Big and fancy
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
