47023a155c0d60810e1fbe2c6f07078a10e504e1
iamdavidzeng <15779712170@163.com>
2021-11-05 19:10:24 +0800

    run async program

--- python/src/iadz/built_in/async/example.py
+++ python/src/iadz/built_in/async/example.py
@@ -1,10 +1,46 @@
 import asyncio
 
-
+# asyncio.run
 async def main():
     print("hello")
-    await asyncio.sleep(1)
     print("world")
+
+
+asyncio.run(main())
+
+
+# await
+import time
+
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, "hello")
+    await say_after(2, "world")
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+asyncio.run(main())
+
+
+# asyncio.create_task
+async def main():
+    task1 = asyncio.create_task(say_after(1, "hello"))
+    task2 = asyncio.create_task(say_after(2, "world"))
+
+    print(f"started at {time.strftime('%X')}")
+
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
 
 
 asyncio.run(main())
