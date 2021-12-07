c515b919d3928d7e5aec4cf4f427bb199beea351
iamdavidzeng <15779712170@163.com>
2021-12-07 19:01:46 +0800

    feat(chat): build chat example

--- web/vite-tutorial/src/App.vue
+++ web/vite-tutorial/src/App.vue
@@ -1,21 +1,55 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue';
+import List from './components/List.vue';
+import Text from './components/Text.vue';
+import Message from './components/Message.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <div class="sidebar">
+      <Card />
+      <List />
+    </div>
+    <div class="main">
+      <Message />
+      <Text />
+    </div>
+  </div>
 </template>
 
-<style>
+<style lang="less" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 20px auto;
+  width: 800px;
+  height: 600px;
+  overflow: hidden;
+  border-radius: 3px;
+  .sidebar,
+  .main {
+    height: 100%;
+  }
+  .sidebar {
+    float: left;
+    width: 200px;
+    color: #f4f4f4;
+    background-color: #2e3238;
+  }
+  .main {
+    position: relative;
+    overflow: hidden;
+    background-color: #eee;
+  }
+  .text {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+  }
+  .message {
+    height: ~"calc(100% - 160px)";
+  }
 }
 </style>
---
+++ web/vite-tutorial/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+const state = {};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+export const store = createStore({
+    state,
+    getters,
+    actions,
+    mutations
+});
